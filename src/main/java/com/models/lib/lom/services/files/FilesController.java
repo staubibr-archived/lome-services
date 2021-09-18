eb7e145e85a9c52e2d1bd8ef210b92691c25504d
staubibr <staubin.bruno@gmail.com>
2021-09-17 22:59:46 -0400

    Merged upload controller with file controller

--- src/main/java/com/models/lib/lom/services/files/FilesController.java
+++ src/main/java/com/models/lib/lom/services/files/FilesController.java
@@ -1,8 +1,10 @@
 package com.models.lib.lom.services.files;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.models.lib.lom.components.Query;
 
@@ -28,6 +32,17 @@ public class FilesController {
     public List<Object> create(@RequestBody List<Files> entities) {
     	return service.create(entities);
     }
+
+	@PostMapping(path = "api/files/upload", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+	public List<Long> uploadFile(@RequestPart("files") List<MultipartFile> files, @RequestPart String db_files) throws IOException{
+		List<Files> l_db_files = this.service.parse(db_files);
+		
+		String message = this.service.validate(files, l_db_files);
+		
+		if (message != null) throw new IllegalArgumentException(message) ;
+		
+		return this.service.upload(files, l_db_files);
+	}
     
     @GetMapping("/api/files/{id}")
     public Files get(@PathVariable(value = "id") Long id,
--- src/main/java/com/models/lib/lom/services/files/FilesService.java
+++ src/main/java/com/models/lib/lom/services/files/FilesService.java
@@ -1,8 +1,20 @@
 package com.models.lib.lom.services.files;
 
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import com.models.lib.lom.components.Dao;
 import com.models.lib.lom.components.Query.Comparator;
 import com.models.lib.lom.components.Service;
@@ -13,6 +25,8 @@ import com.models.lib.lom.services.file_types.FileTypesTable;
 
 @Repository
 public class FilesService extends Service<Files> {
+
+	static final String FOLDER = "D:\\Development\\arslab-services-v2\\files\\";
 
 	@Autowired
     private FileTypesService sFileTypes;
@@ -30,4 +44,43 @@ public class FilesService extends Service<Files> {
 		e.setFile_type(sFileTypes.selectOne(FileTypesTable.colId, Comparator.eq, e.getFile_type_id()));
 		e.setLast_author(sContributors.selectOne(ContributorsTable.colId, Comparator.eq, e.getLast_author_id()));
 	}
+	
+	public List<Files> parse(String db_files) throws JsonMappingException, JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+	    CollectionType clss = mapper.getTypeFactory().constructCollectionType(List.class, Files.class);
+
+	    return mapper.readValue(db_files, clss);
+	}
+	
+	public String validate(List<MultipartFile> files, List<Files> db_files) {
+		if (files.isEmpty()) return "Empty list of files provided.";
+
+		if (db_files.isEmpty()) return "Empty list of database files provided.";
+
+		if (db_files.size() != files.size()) return "Length mismatch between the ids and files provided.";
+
+		for (int i = 0; i < files.size(); i++) { 
+			if (!files.get(i).getOriginalFilename().equals(db_files.get(i).getName())) {
+				return "File name mismatch between between files provided.";
+			}
+		}
+		
+		return null;
+	}
+	
+    private void copyFile(MultipartFile f, BigInteger id) throws IOException {
+		String name = FOLDER + id.toString() + "_" + f.getOriginalFilename();
+		
+		java.nio.file.Files.copy(f.getInputStream(), Paths.get(name), StandardCopyOption.REPLACE_EXISTING);
+    }
+    
+    public List<Long> upload(List<MultipartFile> files, List<Files> db_files) throws IOException {
+		List<BigInteger> ids = this.create(db_files).stream().map(id -> (BigInteger)id).collect(Collectors.toList());
+
+		for (int i = 0; i < files.size(); i++) {
+			this.copyFile(files.get(i), ids.get(i));
+		}
+		
+		return ids.stream().map(x -> x.longValue()).collect(Collectors.toList());
+    }
 }
